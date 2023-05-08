2de102ccae421c9ea265b155eb5ef6dea513a99b
brannt <branntart@gmail.com>
2023-05-08 13:17:33 +0200

    Vercel rewrites

--- vercel/api/tgbot.py
+++ vercel/api/tgbot.py
@@ -14,13 +14,12 @@ class handler(BaseHTTPRequestHandler):
             self.send_response(403)
             self.send_header('Content-type','application/json')
             self.end_headers()
-            json.dump(self.wfile, {'statusCode': 403, 'body': 'Forbidden'})
-            self.wfile.write('Hello, world!'.encode('utf-8'))
+            json.dump({'statusCode': 403, 'body': 'Forbidden'}, self.wfile)
             return
         update = json.loads(self.rfile.read(int(self.headers['Content-Length'])).decode('utf-8'))
         response = webhook(UPDATER.bot, update)
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        json.dump(self.wfile, response)
+        json.dump(response, self.wfile)
         return
