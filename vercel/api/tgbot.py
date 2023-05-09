7d50e47ed1fd6863a7d0259e6933f22cdd3288d2
brannt <branntart@gmail.com>
2023-05-09 21:49:22 +0200

    Change vercel token handling

--- vercel/api/tgbot.py
+++ vercel/api/tgbot.py
@@ -1,4 +1,5 @@
 import json
+import urllib
 from http.server import BaseHTTPRequestHandler
 from summarizer.integrations.telegram import get_updater, webhook
 
@@ -9,7 +10,16 @@ UPDATER = get_updater()
 class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
-        caller_token = self.path.split('/')[-1]
+        query = urllib.parse.urlparse(self.path).query
+        query_params = urllib.parse.parse_qs(query)
+        if 'token' in query_params:
+            caller_token = query_params['token'][0]
+        else:
+            self.send_response(400)
+            self.end_headers()
+            json.dump({'statusCode': 403, 'body': 'Missing `token` parameter'}, self.wfile)
+            return
+
         if caller_token != UPDATER.bot.token:
             self.send_response(403)
             self.send_header('Content-type','application/json')
